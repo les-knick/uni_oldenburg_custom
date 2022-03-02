3e36c0d5e37b8cc96d3b65f0be9bd7d0f0f86cef
les-knick <lea.stocker@knickdesign.de>
2022-03-02 11:59:12 +0100

    responsive plus hero header

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,9 @@ window.onscroll = function() {
 };
 
 document.addEventListener("DOMContentLoaded", changeHomeLogo);
+document.addEventListener("DOMContentLoaded", noLinksInLinkblock);
+document.addEventListener("DOMContentLoaded", getSearchString);
+
 
 function changeHomeLogo() {
     jQuery(document).ready(function($) {
@@ -19,6 +22,46 @@ function changeHomeLogoOnScroll() {
             $(".site-header--frontpage .site-branding__logo img").attr("src", "themes/custom/uni_oldenburg_custom/assets/img/logo-onwhite.jpg");
         } else {
             $(".site-header--frontpage .site-branding__logo img").attr("src", "themes/custom/uni_oldenburg_custom/assets/img/logo-free.png");
+        }
+    });
+}
+
+function noLinksInLinkblock() {
+    jQuery(document).ready(function($) {
+        var link_block = $('.region--link-block');
+        var link_block__content = $('.region--link-block .block__content a');
+        if (link_block__content[0]) {
+            //content vorhanden
+        } else {
+            //kein content
+            link_block.css('display', 'none');
+        }
+    });
+}
+
+function storeSearchString() {
+    jQuery(document).ready(function($) {
+        var storedSearchString = document.getElementById("searchString");
+        localStorage.setItem("searchString", storedSearchString.value);
+        window.location.href = "/volltextsuche";
+    });
+}
+
+
+function getSearchString() {
+    jQuery(document).ready(function($) {
+        var searchInputField = document.getElementById("edit-search-api-fulltext");
+        var searchString = localStorage.getItem("searchString");
+
+        if (searchString != "alreadySearchedThis" && searchString) {
+            searchInputField.value = searchString;
+
+            localStorage.setItem("searchString", "alreadySearchedThis");
+
+            var submitBtn = document.getElementById('views-exposed-form-duplicate-of-volltextsuche-page-1');
+            submitBtn.submit();
+        } else if (!searchedString) {
+            searchInputField.value = "";
         }
     });
 }
